fbf331624bbe7f5d3e2ededdc620daac0db6504b
James-Oswald <jamesoswald111@gmail.com>
2023-02-17 13:27:50 -0500

    More failed attempts at ex2

--- src/ALCexample2.lean
+++ src/ALCexample2.lean
@@ -12,12 +12,31 @@ show B, from
       have h3 : ¬ A, from h h2,
       show false, from h3 h1)
 
-
-theorem example2 : ∀ KB R o1 o2, (entails KB (RoleAssertion R (o1, o2)) ↔ (RoleAssertion R (o1, o2)) ∈ KB) := 
+theorem example2 : ∀ (Δi : set DomainType) (Iac : AtomicConceptType -> set DomainType)
+(Iar : AtomicRoleType -> set (DomainType × DomainType)) (Io : IndividualType -> DomainType), 
+∀ KB R o1 o2,
+(entails KB (RoleAssertion R (o1, o2)) ↔ (RoleAssertion R (o1, o2)) ∈ KB) := 
 begin
+  intros Δi, intros Iac, intros Iar, intros Io,
   intros KB, intros R, intros o1, intros o2,
-  apply iff.intro,{
-    sorry,
+  apply iff.intro,
+  {
+    --intros H,
+    --rewrite entails at H,
+    --specialize H Δi Iac Iar Io,
+    --apply contraposition at H,
+    --by_cases (∀ (s : ALCStatement), s ∈ KB → eval Δi Iac Iar Io s),
+    --{
+    --  apply H at h,
+    --  sorry,
+    --},
+    --{
+    --  sorry,
+    --}
+    --rewrite entails at H,
+    --specialize H Δi Iac Iar Io,
+    --by_contra,
+    --by_cases ,
   },
   {
     intros H,
